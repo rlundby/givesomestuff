28bd48fa8b7de096e4b6526751621d2bd596c50d
rlundby <rickardlundby@gmail.com>
2018-10-04 23:47:52 +0200

    initital commit

---
+++ givestuff/src/Mock/Organisations.js
@@ -0,0 +1,29 @@
+const Organisations = {
+    animals: {
+        djurensratt: {
+            name: 'Djurens rätt',
+            desc: "Info om djurens rätt"
+        },
+        wwf: {
+            name: 'Naturvärldsfonden',
+            desc: "fonden"
+        }
+    },
+    people: {
+
+    },
+    sickness: {
+
+    },
+    violence: {
+
+    },
+    culture: {
+
+    },
+    misc: {
+
+    }
+};
+
+export {Organisations};
